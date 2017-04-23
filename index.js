5eb56c189cb467b8b7dbb51ae1b8273040d3f4f8
Seth L. Blumberg <sethb@pobox.com>
2017-04-23 18:42:56 -0400

    rename export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const yaml = require('js-yaml');
 
-module.exports = yaml.Schema.create([yaml.DEFAULT_SAFE_SCHEMA], [
+module.exports.SCHEMA = yaml.Schema.create([yaml.DEFAULT_SAFE_SCHEMA], [
   new yaml.Type('tag:yaml.org,2002:set', {
     kind: 'mapping',
     construct: (data) => new Set(Object.keys(data || {})),
