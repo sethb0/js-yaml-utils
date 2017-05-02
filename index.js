bbad0e392f3094679f5d4b2b011ba03a6f5710e9
Seth L. Blumberg <sethb@pobox.com>
2017-05-01 23:39:35 -0400

    add some frequently-used functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const Promise = require('bluebird');
+const fs = require('fs');
 const yaml = require('js-yaml');
 
-module.exports.SCHEMA = yaml.Schema.create([yaml.DEFAULT_SAFE_SCHEMA], [
+const SCHEMA = yaml.Schema.create([yaml.DEFAULT_SAFE_SCHEMA], [
   new yaml.Type('tag:yaml.org,2002:set', {
     kind: 'mapping',
     construct: (data) => new Set(Object.keys(data || {})),
@@ -14,3 +16,23 @@ module.exports.SCHEMA = yaml.Schema.create([yaml.DEFAULT_SAFE_SCHEMA], [
     },
   }),
 ]);
+module.exports.SCHEMA = SCHEMA;
+
+module.exports.loadSync = function loadSync (path) {
+  return yaml.safeLoad(fs.readFileSync(path, { encoding: 'utf8' }),
+                       { schema: SCHEMA, filename: path });
+};
+
+module.exports.dumpSync = function dumpSync (path, data) {
+  fs.writeFileSync(path,
+                   yaml.safeDump(data, { schema: SCHEMA, styles: { '!!null': 'canonical' } }),
+                   'utf8');
+};
+
+const writeFile = Promise.promisify(fs.writeFile);
+
+module.exports.dump = function dump (path, data) {
+  return writeFile(path,
+                   yaml.safeDump(data, { schema: SCHEMA, styles: { '!!null': 'canonical' } }),
+                   'utf8');
+};
